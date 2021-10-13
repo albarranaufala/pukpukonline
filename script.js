e7cc33fe9892724a6095978b32ec4784c3500bf8
Albarra Erdanto <albarraerdanto@gmail.com>
2021-10-13 21:29:28 +0700

    change 3 pukpuk each pukpuk

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ const socket = io('https://pukpuk-api.herokuapp.com');
 
 const pukpukButton = document.getElementById('pukpukButton');
 
-const pukpuk = () => {
+const pukpuk = async () => {
     console.log('puk puk');
-    const duration = 200;
+    const duration = 250;
+    const gap = 100;
+    navigator.vibrate(duration);
+    await delay(duration);
+    await delay(gap);
+    console.log('puk puk');
+    navigator.vibrate(duration);
+    await delay(duration);
+    await delay(gap);
+    console.log('puk puk');
     navigator.vibrate(duration);
 }
 
@@ -17,11 +26,13 @@ socket.on('pukpuk', ({ data }) => {
 })
 
 const handleReceivePukpuk = () => {
+    pukpuk();
     const pukpuks = document.getElementById('pukpuks');
     if (pukpuks.innerText === 'Belum puk puk') {
         pukpuks.innerHTML = '<li class="my-2 font-bold text-2xl text-center">Puk puk puk</li>';
     } else {
         pukpuks.innerHTML = '<li class="my-2 font-bold text-2xl text-center">Puk puk puk</li>' + pukpuks.innerHTML;
     }
-    pukpuk();
 }
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
