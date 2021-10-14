0b5f1b3ee31d6d98e03cf87aaca65963475deeae
Albarra Erdanto <albarraerdanto@gmail.com>
2021-10-14 11:02:36 +0700

    fix layout

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// const socket = io('https://pukpuk-api.herokuapp.com');
-const socket = io('http://localhost:3000');
+const socket = io('https://pukpuk-api.herokuapp.com');
 
 const pukpukButton = document.getElementById('pukpukButton');
 
